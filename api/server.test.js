c543821cc0b153fb2c5106d24dae9a20c935346a
canasmh <canasmh@yahoo.com>
2023-01-20 16:05:14 -0500

    Write some tests for the register route

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,39 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const server = require('./server.js');
+const request = require('supertest');
+const db = require('../data/dbConfig');
+
+beforeEach(async () => {
+  await db('users').truncate();
+})
+
+afterAll(async () => {
+  await db.destroy();
+})
+describe('testing the api endpoints', () => {
+  
+  describe('testing the auth register endpoint', () => {
+    it('missing password returns "username and password required"', async () => {
+      const response = await request(server).post('/api/auth/register').send({username: 'manny'})
+      expect(response.text).toBe('username and password required');
+    });
+
+    it('check that new user is added to the database', async () => {
+      let users = await db('users');
+      expect(users).toHaveLength(0);
+      const response = await request(server).post('/api/auth/register').send({username: 'manny', password: 'mypassword'})
+      users = await db('users');
+      expect(users).toHaveLength(1);
+    })
+
+    it('check that user is not added if username exists', async () => {
+      const response = await request(server).post('/api/auth/register').send({username: 'manny', password: 'mypassword'});
+      let users = await db('users');
+      expect(users).toHaveLength(1);
+      const newResponse = await request(server).post('/api/auth/register').send({username: 'manny', password: 'mypassword'});
+      expect(newResponse.text).toBe('username taken');
+      users = await db('users');
+      expect(users).toHaveLength(1);
+    })
+  })
 })
