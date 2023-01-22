ccc83c0845a834fa5c54ff70c681c52e3caa2ffb
canasmh <canasmh@yahoo.com>
2023-01-22 14:15:56 -0500

    Create a 'rollback' script in package.json and use doublequotes in middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -4,7 +4,7 @@ const db = require('../../data/dbConfig');
 function checkBodyParams(req, res, next) {
     if (!req.body.username || !req.body.password) {
         console.log('username or password missing')
-        res.status(400).send('username and password required');
+        res.status(400).send("username and password required");
     } else {
         console.log('username and password provided')
         next();
@@ -17,7 +17,7 @@ async function checkUniqueUsername(req, res, next) {
 
     if (user) {
         console.log('user is taken')
-        res.status(400).send('username taken');
+        res.status(400).send("username taken");
     } else {
         console.log('user is not taken')
         next();
